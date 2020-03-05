26d3f4e16510de46c61879c808e5d61ab556dd72
LittleFogCat <littlefogcat@foxmail.com>
2020-03-05 13:22:41 +0800

    Site updated: 2020-03-05 13:22:40

--- js/page_view.js
+++ js/page_view.js
@@ -5,5 +5,5 @@ function handleCallback(jsonData) {
 	document.getElementById("page_view").innerHTML = arrival;
 }
 
-var jsonpScript = "<script type=\"text/javascript\" src=\"http://106.14.152.93/third/githubio/arrival?callback=handleCallback&articleUrl=" + href + "\"><\/script>";
+var jsonpScript = "<script type=\"text/javascript\" src=\"http://106.14.152.93:8443/third/githubio/arrival?callback=handleCallback&articleUrl=" + href + "\"><\/script>";
 document.write(jsonpScript);
