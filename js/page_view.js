d325ba3f9a57cdf932e2edcc607a88642bc14720
LittleFogCat <littlefogcat@foxmail.com>
2020-03-05 12:25:48 +0800

    Site updated: 2020-03-05 12:25:48

---
+++ js/page_view.js
@@ -0,0 +1,11 @@
+			var href = window.location.href;
+
+			function handleCallback(jsonData) {
+				var arrival = jsonData.arrival;
+				document.getElementById("page_view").innerHTML = arrival;
+			}
+
+			var jsonpScript =
+				"<script type=\"text/javascript\" src=\"http://106.14.152.93/third/githubio/arrival?callback=handleCallback&articleUrl=" +
+				href + "\"><\/script>";
+			document.write(jsonpScript);
